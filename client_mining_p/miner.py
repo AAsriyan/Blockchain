7a4202d52e40fbfd3d61c6a8beaebc4125ed0577
AAsriyan <aasriyan@protonmail.com>
2019-07-11 18:23:52 -0400

    finished client mining MVP

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -11,7 +11,7 @@ def valid_proof(last_proof, proof):
         """
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
+        return guess_hash[:6] == "000000"
 
 def proof_of_work(get_last_proof):
     """
@@ -20,11 +20,11 @@ def proof_of_work(get_last_proof):
     zeroes, where p is the previous p'
     - p is the previous proof, and p' is the new proof
     """
-
+    print("Starting search for new proof")
     proof = 0
     while valid_proof(get_last_proof, proof) is False:
         proof += 1
-
+    print("Found new proof: " + str(proof))
     return proof
 
 # TODO: Implement functionality to search for a proof 
@@ -41,25 +41,27 @@ if __name__ == '__main__':
     # Run forever until interrupted
     while True:
         # TODO: Get the last proof from the server and look for a new one
-        get_last_proof = requests.get(f"{node}/last_proof").json()
+        r = requests.get(url=f"{node}/last_proof")
+        data = r.json()
+        print(data)
+        last_proof = data['proof']
 
-        new_proof = proof_of_work(get_last_proof)
+        new_proof = proof_of_work(last_proof)
         print(new_proof)
-        # TODO: When found, POST it to the server {"proof": new_proof}
-        response = requests.post(url=f"{node}/mine", json={'proof': new_proof})
-        # print(response.message)
 
-        data = response.json()
+        # TODO: When found, POST it to the server {"proof": new_proof}
+        payload = {'proof': new_proof}
+        r = requests.post(url=f"{node}/mine", json=payload)
+        data = r.json()
+
         print(data)
 
         # TODO: If the server responds with 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        # if response['message'] == 'New Block Forged':
-        #     coins_mined += 1
-        #     print(coins_mined)
-        # else:
-        #     message = {
-        #         'message': 'Something wrong happened'
-        #     }
-        #     jsonify(message), 400
+        if data['message'] == 'New Block Forged':
+            coins_mined += 1
+            print(coins_mined)
+        else:
+            print(data['message'])
+
